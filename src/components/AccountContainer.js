761afd17848801f228967509400ca4afb417a9dc
Henry <henrysungyun@gmail.com>
2023-03-02 09:28:04 -0800

    submission for deadline

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+const transactionAPI = " http://localhost:8001/transactions"
 
 function AccountContainer() {
+
+  const[transactions, setTransactions] = useState([])
+  const[filteredTransaction, setFilteredTransaction] = useState("")
+
+  useEffect(() => {
+    fetch(transactionAPI)
+      .then(res => res.json())
+      .then(setTransactions)
+  }, [])
+
+  function renderNewTransaction(date, description, category, amount) {
+    const transactionObj = {
+      date: date,
+      description: description,
+      category: category,
+      amount: amount
+    };
+    fetch(transactionAPI, {
+      method: "POST",
+      headers: {
+        Accepts: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(transactionObj)
+    })
+    .then(res => res.json())
+    .then(transaction => setTransactions([...transactions, transaction]))
+  }
+
+  const filteredTransactionList = transactions.filter((transaction) => transaction.description.toUpperCase().includes(filteredTransaction.toUpperCase()))
+
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList />
+      <Search filteredTransaction={filteredTransaction} setFilteredTransaction={setFilteredTransaction} />
+      <AddTransactionForm renderNewTransaction={renderNewTransaction} />
+      <TransactionsList transactions={filteredTransactionList} />
     </div>
   );
 }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddTransactionForm() {
+function AddTransactionForm({ renderNewTransaction }) {
+
+  const[date, setDate] = useState(new Date())
+  const[description, setDescription] = useState("");
+  const[category, setCategory] = useState("")
+  const[amount, setAmount] = useState(0)
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    renderNewTransaction(date, description, category, amount)
+    setDescription(new Date())
+    setDescription("")
+    setCategory("")
+    setAmount(0)
+  }
+
   return (
     <div className="ui segment">
-      <form className="ui form">
+      <form className="ui form" onSubmit={handleSubmit}>
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input 
+            type="date" 
+            name="date"
+            value={date} 
+            onChange={(e) => setDate(e.target.value)}
+          />
+          <input 
+            type="text" 
+            name="description" 
+            placeholder="Description" 
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input 
+            type="text" 
+            name="category" 
+            placeholder="Category" 
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+          <input 
+            type="number" 
+            name="amount" 
+            placeholder="Amount" 
+            step="0.01" 
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
         </div>
         <button className="ui button" type="submit">
           Add Transaction
